bbc757a7d98244362fbab847d60737fd6d3b7b5c
Luca Piola <lpiola.97@gmail.com>
2021-01-29 17:39:35 +0100

    Add files via upload

---
+++ liucAPI.py
@@ -0,0 +1,172 @@
+import requests
+import json 
+from lxml import html
+from lxml import etree
+from bs4 import BeautifulSoup
+import time
+import numpy
+import getpass 
+import os
+clear = lambda: os.system('cls')
+import json
+
+""" 
+Most of these functions work through REST APIs, but due to lack of documentation about some features,
+this script uses also HTTP request scraping (for example in def get_last_mark()) 
+this is the cause for having two functions for logging in, liucLogin() for loggin through http requests
+while login() for logging through REST APIs
+further investigation in APIs documentation should fix this and make the script work ONLY through REST API. 
+Function get_last_mark() works through http scraping, and it requires liucLogin()
+Please note that the script is working only with students' account. """
+
+#not all urls are from official REST API's endpoints
+url_login = "https://sol.liuc.it/esse3/auth/Logon.do"
+url_esiti = 'https://sol.liuc.it/esse3/auth/studente/Appelli/BachecaEsiti.do'
+url_appelli = 'https://sol.liuc.it/e3rest/api/calesa-service-v1/appelli/'
+url_login_end = "https://sol.liuc.it/e3rest/api/login/"
+#average endpoint = url_average + matId (get this from login()) + "/medie"
+url_average = 'http://sol.liuc.it/e3rest/api/libretto-service-v2/libretti/'
+#example: ".../e3rest/api/libretto-service-v2/libretti/999/medie" return average of student with matId 999
+url_libretto = 'http://sol.liuc.it/e3rest/api/libretto-service-v2/libretti/'
+
+#start requests session
+session = requests.session()
+session.get(url_login)
+
+#login through API
+#return basic info about the student
+def login(username1, pwd):
+    response = session.get(url_login_end, auth=(username1, pwd))
+    user_details_json = json.loads(response.text)
+    user_details = []
+    matId = (user_details_json["user"]["trattiCarriera"][0]["matId"])
+    stuId = (user_details_json["user"]["trattiCarriera"][0]["stuId"])
+    matricola = (user_details_json["user"]["trattiCarriera"][0]["matricola"])
+    name = (user_details_json["user"]["firstName"])
+    surname = (user_details_json["user"]["lastName"])
+    user_details.append(matId)
+    user_details.append(stuId)
+    user_details.append(matricola)
+    user_details.append(name)
+    user_details.append(surname)
+    return user_details
+
+
+
+#return a matrix with available exams and their details
+#this function works through JSON REST API
+def getAppelli(username1, pwd):
+    appelli = session.get(url_appelli, auth=(username1, pwd))
+    appelli_json = json.loads(appelli.text)
+    appelli_detail = [[]]
+    advanced_details_exam = [[]]
+    #look for exam attributes, so i can search for exams description
+    #first endpoints = exam id
+    #second endopoints = input(exam_id)->output(exam_details)
+    for i in range(len(appelli_json)):
+        id_appello = appelli_json[i]["adDefAppId"]
+        id_corso = appelli_json[i]["cdsDefAppId"]
+        desc_appello = appelli_json[i]["adDes"]
+        appelli_detail.insert(i, [desc_appello, id_appello, id_corso])
+        
+    #look for exam details, giving as input exam id 
+    for i in range(len(appelli_detail) - 1):
+        detail_endpoints = url_appelli + str(appelli_detail[i][2]) + "/" +  str(appelli_detail[i][1])
+        get_exam_info = session.get(detail_endpoints, auth=(username1, pwd))
+        exam_info_json = json.loads(get_exam_info.text) 
+        """ print(exam_info_json)
+        print(detail_endpoints) """
+
+        for j in range(len(exam_info_json) - 1):
+            corso = exam_info_json[j]["adDes"]
+            data_appello = exam_info_json[j]["dataInizioApp"]
+            data_inizio = exam_info_json[j]["dataInizioIscr"]
+            data_fine = exam_info_json[j]["dataFineIscr"]
+            tipo_appello = exam_info_json[j]["desApp"]
+            advanced_details_exam.insert((j+i), [corso, data_appello, tipo_appello, data_inizio, data_fine])
+
+    return advanced_details_exam
+
+
+#return average and most likely graduation grade
+def get_media(username1, pwd):
+    matricola_id = login(username1, pwd)[0]
+    personal_url_average = url_average + str(matricola_id) + "/medie"
+    getAverage = session.get(personal_url_average, auth=(username1,pwd))
+    average_json = json.loads(getAverage.text)
+    average = average_json[1]["media"]
+    votolaurea = average_json[3]["media"]
+    return average, votolaurea
+
+
+
+#return a matrix in which each line contains [exam name, exam grade]
+#if an exam has not a grade, return [exam name, "---"]
+def get_libretto(username1, pwd):
+    libretto = [[]]
+    matricola_id = login(username1, pwd)[0]
+    personal_url_libretto = url_libretto + str(matricola_id) + "/righe/"
+    response = session.get(personal_url_libretto, auth = (username1, pwd))
+    libretto_json = json.loads(response.text)
+    num_esami_da_dare = 0
+    for i in range(len(libretto_json)):
+        
+        esame_libretto = libretto_json[i]["adDes"]
+        voto_libretto = libretto_json[i]["esito"]["voto"]
+        if voto_libretto == None:
+            voto_libretto = "---"
+            num_esami_da_dare = num_esami_da_dare + 1
+        libretto.insert(i, [esame_libretto, voto_libretto])
+
+    #adding info about how many exam are finished
+    num_esami_dati = len(libretto_json) - num_esami_da_dare
+    #insert the info in last line of the list
+    esami_dati_da_dare = [num_esami_dati, num_esami_da_dare]
+
+    return libretto, esami_dati_da_dare
+    
+#----------------------------------------------------------------------------------------------------------------
+
+def liucLogin(username1, pwd):
+    response = session.get(url_login, auth=(username1, pwd))
+    #salvo la pagina di scelta carriera
+    tree = etree.HTML(response.text)
+    element = tree.xpath('//*[@id="gu_toolbar_sceltacarriera"]')
+    try:
+        content = etree.tostring(element[0])
+        url1 = content[108:113].decode('utf-8')
+        print("Accedo all'ultima carriera disponibile...") 
+        url_carriera = "https://sol.liuc.it/esse3/auth/studente/SceltaCarrieraStudente.do?stu_id=" + url1
+        response = session.get(url_carriera, auth=(username1, pwd))
+        if (response.status_code) == 200:
+            print("Login riuscito. ") 
+        else:
+            print("Login non riuscito. ")
+    except:
+        print("Login non riuscito ")
+
+
+
+#check the last grades
+def get_last_mark(username1, pwd):
+    response = session.get(url_esiti, auth=(username1,pwd))
+    html_esiti = BeautifulSoup(response.text, "html.parser")
+
+    #nome deve essere fixato, non trovo il css selector esatto, non funziona neanche con xpath
+    #controllare documentazione e3rest su esse3 per api json
+    prof_esame_esito = html_esiti.select('td.detail_table:nth-child(3)')
+    data_esame_esito = html_esiti.select('td.detail_table:nth-child(1)')
+    voto_esame_esito = html_esiti.select('td.detail_table:nth-child(5) > form:nth-child(1)')
+    print(len(prof_esame_esito))
+    esiti = []
+    quanti_esiti = len(prof_esame_esito)
+
+    for i in range(quanti_esiti):
+        prof_esame_esito1 = prof_esame_esito[i].get_text()
+        data_esame_esito1 = data_esame_esito[i].get_text()
+        voto_esame_esito1 = voto_esame_esito[i].get_text()
+        info_esito = prof_esame_esito1 + " - " + data_esame_esito1 + " - " + voto_esame_esito1
+        info_esito = info_esito.replace("\n", "")
+        esiti.append(info_esito)
+  
+    return esiti
---
+++ main.py
@@ -0,0 +1,224 @@
+from getpass import getpass
+import time
+import os
+import shutil
+import logging
+import liucAPI as LIUC
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+LOGIN, INSERT_USER, INSERT_PASSWORD, ACTIVITIES,  BIO = range(5)
+
+username_esse3 = ""
+password_esse3 = ""
+thumbs_up_emoji = u'\U0001F44D' 
+
+def start(update: Update, context: CallbackContext) -> int:
+    reply_keyboard = [['Login']]
+    
+    update.message.reply_text(
+        u'\U0001F393' 
+        ' Benvenuto in Tele-LIUC '
+        u'\U0001F393' 
+        '\n\nQuesto BOT non è ufficiale! \n'
+        'Clicca su LOGIN per entrare '
+        
+        ,
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    )
+   
+           
+ 
+
+    return LOGIN
+
+#ask for username
+def login(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("Choose of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text(
+        'Inserisci l\'username:  (es. pi69.pinguino)',
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return INSERT_USER
+
+#save username and ask password 
+def insert_user(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("Username of %s: %s", user.first_name, update.message.text)
+    username_esse3 = update.message.text
+    context.user_data['user'] = username_esse3 #add entered username to global context
+    update.message.reply_text(
+        'Inserisci la password: (non verrà salvata)'
+    )
+
+    return INSERT_PASSWORD
+
+#save password and ask next operation (get grades, available exams, average...)
+def insert_password(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    #print(context.user_data['user'])
+    logger.info("Password of %s: %s", user.first_name, update.message.text)
+    password_esse3 = update.message.text
+    context.user_data['pass'] = password_esse3 #add entered pass to global context
+    """ update.message.reply_text(
+        "Entro in sol.liuc.it... "
+    ) """
+    user = context.user_data['user']
+    password = context.user_data['pass']
+    update.message.delete() #cancella password inserita per sicurezza
+    #connect to esse 3 API
+    try:
+        logged_info = LIUC.login(user, password)
+        name = logged_info[3]
+        surname = logged_info[4]
+    except:
+        print("Credenziali errate: ")
+        reply_keyboard = [["/start"]]
+        update.message.reply_text("Credenziali errate, clicca su /start per riprovare.",
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),)
+        return ConversationHandler.END
+ 
+    welcome_message = "Ciao " + name + " " + ", scegli un'opzione: "
+    #print on telegram possible choices 
+    reply_keyboard = [['Esiti', 'Libretto', 'Appelli', 'Media', 'Chiudi']]
+    update.message.reply_text(
+    welcome_message,
+    reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+)
+
+    
+    return ACTIVITIES
+
+
+def activities(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    username1 = context.user_data['user']
+    password = context.user_data['pass']
+    
+    url = "https://sol.liuc.it/e3rest/api/"
+    
+    choice = update.message.text
+    
+
+    if choice == 'Libretto':
+        lista_libretto = LIUC.get_libretto(username1, password)[0]
+        numero_esami_dati = LIUC.get_libretto(username1, password)[1]
+        msg_libretto = ""
+        for i in range(len(lista_libretto)-1):
+            msg_libretto = msg_libretto + "\n\n*" + str(lista_libretto[i][0]) + "* - " + str(lista_libretto[i][1])
+        
+        msg_libretto = msg_libretto + "\n\n" + "Esami dati: " + str(numero_esami_dati[0]) + "\nEsami da dare: " + str(numero_esami_dati[1])
+        update.message.reply_text(msg_libretto, parse_mode='Markdown')
+
+
+    LIUC.liucLogin(username1, password)
+
+    #print last marks
+    if choice == 'Esiti':
+        LIUC.liucLogin(username1, password)
+
+        try:
+            
+            esito = LIUC.get_last_mark(username1, password)
+            for i in range(len(esito)):
+                invia_esito = u'\U0001F393' + " " + esito[i]
+                update.message.reply_text(invia_esito)
+        except:
+            #da fixare: se si cerca due volte gli esiti non ne trova
+            update.message.reply_text("Impossibile trovare esiti, riprova. ")
+        
+
+    #print next available exams
+    if choice == 'Appelli':
+
+        lista_appelli = ""
+        #get list with all the exams
+        print_appelli = (LIUC.getAppelli(username1, password))
+        
+        for i in range(len(print_appelli)):
+           
+            try:
+                if int((print_appelli[i][1])[6:10]) >= 2021: #if it's an old exam, it won't be sent 
+                    lista_appelli = lista_appelli + "\n\n *" + print_appelli[i][0] + "* \n" + print_appelli[i][2] + "\n" + (print_appelli[i][1])[0:10]  
+                    #if open and closing dates for exam application are needed, add in lista_appelli 
+                    #this string:
+                    #   + (print_appelli[i][3])[0:10]  + (print_appelli[i][4])[0:10] 
+            except:
+                print("Empty array ") #da cancellare era solo per debug
+        update.message.reply_text(lista_appelli, parse_mode='Markdown')
+    
+    #print grades' average
+    if choice == 'Media':
+        try:
+            media = LIUC.get_media(username1, password)[0]
+            voto = LIUC.get_media(username1, password)[1]
+            return_media_voto = "La tua media è *" + str(media) + "*, il voto di laurea è *" + str(voto)[0:5] + "*. " + thumbs_up_emoji
+            update.message.reply_text(return_media_voto, parse_mode='Markdown')
+        except:
+            #da fixare, problema in liucAPI, probabilmente se logga due volte va a cazzo
+            update.message.reply_text("Impossibile trovare media, riprova. ")
+
+    #stop the conversation
+    if choice == 'Chiudi':
+        update.message.reply_text("Logout effettuato! Scrivi /start per ricominciare!")
+        return ConversationHandler.END
+
+
+    reply_keyboard = [['Esiti', 'Libretto', 'Appelli', 'Media', 'Chiudi']]
+    update.message.reply_text(
+    "Cosa vuoi fare? ",
+    reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+    )
+    return ACTIVITIES
+
+
+
+
+#main bot function to handle the conversation
+def main() -> None:
+    
+    updater = Updater("1492190157:AAHJKgrfxnxFKodOWleARo3hte3DBvuHYyA", use_context=True)
+    dispatcher = updater.dispatcher
+
+
+    # Add conversation handler with the states
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            LOGIN: [MessageHandler(Filters.regex('^(Login)$'), login, pass_user_data=True)],
+            INSERT_USER: [MessageHandler(Filters.text, insert_user, pass_user_data=True)],
+            INSERT_PASSWORD: [
+                MessageHandler(Filters.text, insert_password, pass_user_data=True)],
+            ACTIVITIES: [MessageHandler(Filters.regex('^(Esiti|Libretto|Appelli|Media|Chiudi)$'), activities)],
+        },
+        fallbacks=[CommandHandler('cancel', activities)],
+         allow_reentry = True,
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    #starting bot
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
+
+
